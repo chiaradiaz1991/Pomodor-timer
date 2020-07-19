4cdf401afbd8dc01b162c1492306499d1c0d0ddb
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-07-19 11:50:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pomodoro from './Components/Pomodoro/Pomodoro';
+
+const App = ()=> {
+  return (
+    <div className="App">
+      <Pomodoro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Pomodoro/Pomodoro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Pomodoro = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Pomodoro;
